064a51d5caf27f2c87d5e9445141440929e867a1
FerissaM <mabalotferissa@gmail.com>
2024-05-13 14:37:58 -0700

    homepage

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { getUser } from '../../utilities/users-service';
 import './App.css';
 import AuthPage from '../AuthPage/AuthPage';
+import Homepage from '../Homepage/Homepage';
 import NewOrderPage from '../NewOrderPage/NewOrderPage';
 import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage';
 import NavBar from '../../components/NavBar/NavBar';
@@ -12,18 +13,19 @@ export default function App() {
 
   return (
     <main className="App">
-      { user ?
+      { user ? (
           <>
             <NavBar user={user} setUser={setUser} />
             <Routes>
               {/* Route components in here */}
+              <Route path="/" element={<Homepage />} /> {/* render Homepage at the root */}
               <Route path="/orders/new" element={<NewOrderPage />} />
               <Route path="/orders" element={<OrderHistoryPage />} />
             </Routes>
           </>
-          :
+        ) : (
           <AuthPage setUser={setUser} />
-      }
+      )}
     </main>
   );
 }
---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to ComiCool!</h1>
+      <p>Explore our collection of comics to your pleasure!</p>
+      {/* Add more content */}
+    </div>
+  );
+}
+
+export default HomePage;
